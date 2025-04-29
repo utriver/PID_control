13284908fe723d500cf86c6ee8102c2e500c1d3d
chunggeun.kim <chunggeun.kim@neuromeka.com>
2025-04-29 13:34:07 +0900

    fix minor code issue

--- EthercatCore/dynamic_calculate.py
+++ EthercatCore/dynamic_calculate.py
@@ -219,8 +219,6 @@ for t in range(T_slide):
     m_v = (2.0/ca.pi) * ca.arctan(k_static*slide_velocity[t])
     Fc = ca.if_else(slide_velocity[t] >= 0, Fcp, Fcn)
     s_v_array_slide[t] = Fc * m_v
-    else:
-        s_v_array_slide[t] = Fcn + (Fsn - Fcn) * np.exp(- (abs(slide_velocity[t]) / Vsn) ** delta_sn)
 
 # -------------------------------
 # GMS 모델 함수 (Slide data, k와 alpha는 고정)
@@ -257,6 +255,7 @@ def gms_model_slide(C, fixed_k, fixed_alpha, fixed_sigma, velocity, s_v_array, c
             velocity[t] >= 0, 
             Fv1p*(1-ca.exp(-ca.fabs(velocity[t]/Fv2p)))*ca.sign(velocity[t]),
             Fv1n*(1-ca.exp(-ca.fabs(velocity[t]/Fv2n)))*ca.sign(velocity[t])
+        )
         F_pred[t] = F_t
     return F_pred
 
